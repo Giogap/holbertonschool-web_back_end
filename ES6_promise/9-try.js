af3b038ebbfa1cf044e2d31f6880d543f082904c
Giogap <giogap@gmail.com>
2023-03-25 12:33:49 -0500

    upload 9

--- ES6_promise/9-try.js
+++ ES6_promise/9-try.js
@@ -1,17 +1,17 @@
 function guardrail(mathFunction) {
   const queue = [];
   let result;
-  
+
   try {
     result = mathFunction();
   } catch (e) {
     result = e.toString();
   }
-  
+
   queue.push(result);
   queue.push('Guardrail was processed');
-  
+
   return queue;
 }
 
-  export default guardrail;
+export default guardrail;
