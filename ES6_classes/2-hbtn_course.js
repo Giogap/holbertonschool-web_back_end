e1a1f0819ef1288ea81b047b587d7ea5c172c692
Giogap <giogap@gmail.com>
2023-03-28 00:08:04 -0500

    upload 2

--- ES6_classes/2-hbtn_course.js
+++ ES6_classes/2-hbtn_course.js
@@ -8,4 +8,15 @@ export default class HolbertonCourse {
   get name() {
     return this._name;
   }
+
+  set name(value) {
+    if (typeof value !== 'string') {
+      throw new TypeError('Name must be a string');
+    }
+    this._name = value;
+  }
+
+  get length() {
+    return this._length;
+  }
 }
