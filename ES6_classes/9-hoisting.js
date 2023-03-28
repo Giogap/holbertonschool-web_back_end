8873db035f194f400eeb8671a10e44a45a0b51ee
Giogap <giogap@gmail.com>
2023-03-28 13:32:38 -0500

    upload 9

--- ES6_classes/9-hoisting.js
+++ ES6_classes/9-hoisting.js
@@ -27,21 +27,20 @@ export class StudentHolberton {
     return `${this._firstName} ${this._lastName}`;
   }
   
-    get holbertonClass() {
-      return this._holbertonClass;
-    }
-  
-    get fullStudentDescription() {
-      return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
-    }
+  get holbertonClass() {
+    return this._holbertonClass;
   }
   
-  const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
-  const student2 = new StudentHolberton('John', 'Doe', class2020);
-  const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
-  const student4 = new StudentHolberton('Donald', 'Bush', class2019);
-  const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+  get fullStudentDescription() {
+    return `${this._firstName} ${this._lastName} - ${this._holbertonClass.year} - ${this._holbertonClass.location}`;
+  }
+}
   
-  const listOfStudents = [student1, student2, student3, student4, student5];
-  
-  export default listOfStudents;
+const student1 = new StudentHolberton('Guillaume', 'Salva', class2020);
+const student2 = new StudentHolberton('John', 'Doe', class2020);
+const student3 = new StudentHolberton('Albert', 'Clinton', class2019);
+const student4 = new StudentHolberton('Donald', 'Bush', class2019);
+const student5 = new StudentHolberton('Jason', 'Sandler', class2019);
+const listOfStudents = [student1, student2, student3, student4, student5];
+
+export default listOfStudents;
