76d88135566a7aa6003992e5a28532b2c8d6be51
Giogap <giogap@gmail.com>
2023-03-28 12:36:20 -0500

    upload 5

--- ES6_classes/5-building.js
+++ ES6_classes/5-building.js
@@ -1,7 +1,7 @@
 class Building {
   constructor(sqft) {
     if (this.constructor !== Building && typeof this.evacuationWarningMessage !== 'function') {
-        throw Error('Class extending Building must override evacuationWarningMessage');
+      throw Error('Class extending Building must override evacuationWarningMessage');
     }
 
     this._sqft = sqft;
