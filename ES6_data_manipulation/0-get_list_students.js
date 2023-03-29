e507514da62cbea165ddd788f5ff6b3642414bc9
Giogap <giogap@gmail.com>
2023-03-29 09:46:15 -0500

    upload 0

--- ES6_data_manipulation/0-get_list_students.js
+++ ES6_data_manipulation/0-get_list_students.js
@@ -1,11 +1,11 @@
-const getListStudents = () => {
-  const students = [
-    { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
-    { id: 2, firstName: 'James', location: 'Columbia' },
-    { id: 5, firstName: 'Serena', location: 'San Francisco' },
-  ];
-
-  return students;
-};
-
-export default getListStudents;
+function getListStudents() {
+    const obj = [
+      { id: 1, firstName: 'Guillaume', location: 'San Francisco' },
+      { id: 2, firstName: 'James', location: 'Columbia' },
+      { id: 5, firstName: 'Serena', location: 'San Francisco' },
+    ];
+  
+    return obj;
+  }
+  
+  export default getListStudents;
