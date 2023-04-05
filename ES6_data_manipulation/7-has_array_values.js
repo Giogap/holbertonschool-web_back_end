bee4517fd6d002b12376660947a91e2128dfbe45
Giogap <giogap@gmail.com>
2023-04-05 01:15:40 -0500

    upload 7

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,4 +1,4 @@
 export default function hasValuesFromArray(set, ArrayElement) {
-    const bool = ArrayElement.every((boolean) => set.has(boolean));
-    return (bool);
-  }
+  const bool = ArrayElement.every((boolean) => set.has(boolean));
+  return (bool);
+}
