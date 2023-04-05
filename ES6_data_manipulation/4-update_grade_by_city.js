c3558d2e7ac227c10354505e9a459ae995fc5b66
Giogap <giogap@gmail.com>
2023-04-04 23:24:27 -0500

    upload 4

--- ES6_data_manipulation/4-update_grade_by_city.js
+++ ES6_data_manipulation/4-update_grade_by_city.js
@@ -3,17 +3,18 @@ const updateStudentGradeByCity = (students, city, newGrades) => {
     return [];
   }
 
-const studentByCity = students
+  const studentByCity = students
   .filter((student) => student.location === city)
   .map((student) => {
     const grades = newGrades.filter((note) => student.id === note.studentId);
     let grade = 'N/A';
 
     if (grades[0]) {
-      grade = grades[0].grade;
+    grade = grades[0].grade;
     }
     return { ...student, grade };      
-    });
+  });
   return studentByCity;
 };
+
 export default updateStudentGradeByCity;
