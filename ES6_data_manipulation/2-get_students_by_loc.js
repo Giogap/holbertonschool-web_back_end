5a755185376f0a73920c4dedf7a26271206313d4
Giogap <giogap@gmail.com>
2023-04-04 19:59:27 -0500

    upload 2

--- ES6_data_manipulation/2-get_students_by_loc.js
+++ ES6_data_manipulation/2-get_students_by_loc.js
@@ -1,4 +1,8 @@
-export default function getStudentsByLocation(students, city) {
-  const list = students.filter((value) => value.location === city);
+import getListStudents from "./0-get_list_students.js";
+
+function getStudentsByLocation(students, city) {
+  const list = students.filter(student => student.location === city);
   return list;
 }
+
+const students = getListStudents();
