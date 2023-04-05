f4eb55af4834141ee06743091d68c1b1662852ca
Giogap <giogap@gmail.com>
2023-04-04 23:32:20 -0500

    upload 5

--- ES6_data_manipulation/5-typed_arrays.js
+++ ES6_data_manipulation/5-typed_arrays.js
@@ -1,12 +1,14 @@
-export default function createInt8TypedArray(length, position, value) {
-  if (position < length) {
+const createInt8TypedArray = (length, position, value) => {
     const buffer = new ArrayBuffer(length);
-    const array = new Int8Array(buffer);
-    array[position] = `0x${value.toString(16).toUpperCase()}`;
-    const dataView = new DataView(buffer);
-    return dataView;
-  }
-
-  throw new Error('Position outside range');
-
-}
+    const data = new DataView(buffer);
+  
+    if (position > length - 1) {
+      throw new Error('Position outside range');
+    }
+  
+    data.setInt8(position, value);
+  
+    return data;
+  };
+  
+  export default createInt8TypedArray;
