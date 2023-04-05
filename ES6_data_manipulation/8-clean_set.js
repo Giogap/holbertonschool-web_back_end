cf9448ddf4fa832f65ba2fdb546e3dc8b6740895
Giogap <giogap@gmail.com>
2023-04-05 01:17:45 -0500

    upload 8

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,9 +1,7 @@
 export default function cleanSet(set, startString) {
-    if (!startString || typeof startString !== 'string') {
-      return '';
-    }
-    const list = Array.from(set).filter(
-      (firstHalf) => (firstHalf ? firstHalf.includes(startString) : ''),
-    ).map((secondHalf) => secondHalf.slice(startString.length)).join('-');
-    return (list);
+  if (!startString || typeof startString !== 'string') {
+    return '';
   }
+  const list = Array.from(set).filter((firstHalf) => (firstHalf ? firstHalf.includes(startString) : '')).map((secondHalf) => secondHalf.slice(startString.length)).join('-');
+  return (list);
+}
